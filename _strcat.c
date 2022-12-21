f6ec1b9cdbe189d62d1026ceb74e90e4cb43de45
Oluwafemi <makanjuolafemi17@gmail.com>
2022-12-21 10:36:54 -0800

    append source string to dest string

--- _strcat.c
+++ _strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	n = string_length(dest);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[n + i] = src[i];
+	dest[n + i] = src[i];
 	}
 	dest[n + i] = '\0';
 	return (dest);
